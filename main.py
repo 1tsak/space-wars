fe9dbbd3dbca1ec247524404c8fec43c8e148c69
Aakash Jha <42407874+1tsak@users.noreply.github.com>
2022-10-23 21:19:58 +0530

    Event Loop and quit event trigger

--- main.py
+++ main.py
@@ -25,3 +25,15 @@ RED_LASER = pygame.image.load(os.path.join("assets","pixel_laser_yellow.png"))
 
 BG = pygame.image.load(os.path.join("assets","background-black.png"))
 
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+main()
